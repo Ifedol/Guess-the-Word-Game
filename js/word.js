172ced95004f9f0c2a9dad5bf567df57c4679689
Ifedol <dolapomuizat@gmail.com>
2022-09-02 15:47:49 +0100

    first commit

---
+++ js/word.js
@@ -0,0 +1,170 @@
+const wordList = [
+    {
+        word: "python",
+        hint: "programming language"
+    }, 
+    {
+        word: "guitar",
+        hint: "a musical instrument"  
+    },
+    {
+        word: "aim",
+        hint: "a purpose or intention"
+    },
+    {
+        word: "venus",
+        hint: "planet of our solat system"
+    },
+     {
+        word: "gold",
+        hint: "a yellow precious metal"
+    },
+    {
+        word: "golang",
+        hint: "programming language"
+    },
+    {
+        word: "coding",
+        hint: "related to programming"
+    },
+    {
+        word: "matrix",
+        hint: "science fiction movie"
+    },
+    {
+        word: "bugs",
+        hint: "related to programming"
+    },
+    {
+        word: "avatar",
+        hint: "epic science fiction film"
+    },
+    {
+        word: "joking",
+        hint: "humorous or flippant"
+    },
+    {
+        word: "mental",
+        hint: "relating to the mind"
+    },
+    {
+        word: "map",
+        hint: "diagram representation of area"
+    },
+    {
+        word: "island",
+        hint: "land surrounded by water"
+    },
+    {
+        word: "hockey",
+        hint: "a famous outdoor game"
+    },
+    {
+        word: "chess",
+        hint: "game played by two players"
+    },
+    {
+        word: "github",
+        hint: "code hosting platform"
+    },
+    {
+        word: "png",
+        hint: "type of raster image file"
+    },
+    {
+        word: "silver",
+        hint: "precious greyish-white metal"
+    },
+    {
+        word: "java",
+        hint: "programming language"
+    },
+    {
+        word: "google",
+        hint: "famous search engine"
+    },
+    {
+        word: "banana",
+        hint: "yellow long curved fruit"
+    },
+    {
+        word: "venice",
+        hint: "famous city of waters"
+    },
+    {
+        word: "mysql",
+        hint: "a relational database system"
+    },
+    {
+        word: "nepal",
+        hint: "a developing country"
+    },
+    {
+        word: "flute",
+        hint: "a musical instrument"
+    },
+    {
+        word: "crypto",
+        hint: "related to cryptocurrency"
+    },
+    {
+        word: "tesla",
+        hint: "unit of magnetic flux density"
+    },
+    {
+        word: "rocket",
+        hint: "space vehicle or missile"
+    },
+    {
+        word: "mars",
+        hint: "second smallest planet"
+    },
+    {
+        word: "apple",
+        hint: "mobile manufacture brand"
+    },
+    {
+        word: "proxy",
+        hint: "related to server application"
+    },
+    {
+        word: "email",
+        hint: "related to exchaging message"
+    },
+    {
+        word: "html",
+        hint: "markup language for the web"
+    },
+    {
+        word: "cloud",
+        hint: "condensed watery vapour"
+    },
+    {
+        word: "brake",
+        hint: "make a moving vehicle slow"
+    },
+    {
+        word: "air",
+        hint: "the invisible gaseous"
+    },
+    {
+        word: "idea",
+        hint: "a thought or suggestion"
+    },
+    {
+        word: "server",
+        hint: "related to computer or system"
+    },
+    {
+        word: "svg",
+        hint: "a vector image format"
+    },
+    {
+        word: "jpeg",
+        hint: "a small file format"
+    },
+    {
+        word: "search",
+        hint: "act to find something"
+    }
+]
